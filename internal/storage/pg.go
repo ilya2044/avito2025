f2882f123437731abc05db6426d59ad5de9bbbc9
Петров Илья Олегович <ip217s24@csc.sibsutis.ru>
2025-11-18 21:26:28 +0700

    fix docker

--- internal/storage/pg.go
+++ internal/storage/pg.go
@@ -9,20 +9,22 @@ import (
 )
 
 func NewDBFromEnv() (*sql.DB, error) {
-	host := env("DB_HOST", "db")
-	port := env("DB_PORT", "5432")
-	user := env("DB_USER", "postgres")
-	pass := env("DB_PASS", "postgres")
-	name := env("DB_NAME", "prdb")
-	ssl := env("DB_SSLMODE", "disable")
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", host, port, user, pass, name, ssl)
+	host := getEnv("DB_HOST", "db")
+	port := getEnv("DB_PORT", "5432")
+	user := getEnv("DB_USER", "postgres")
+	pass := getEnv("DB_PASS", "postgres")
+	name := getEnv("DB_NAME", "prdb")
+	ssl := getEnv("DB_SSLMODE", "disable")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host, port, user, pass, name, ssl)
+
 	return sql.Open("postgres", dsn)
 }
 
-func env(key, def string) string {
-	v := os.Getenv(key)
-	if v == "" {
-		return def
+func getEnv(key, defaultValue string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
 	}
-	return v
+	return defaultValue
 }
